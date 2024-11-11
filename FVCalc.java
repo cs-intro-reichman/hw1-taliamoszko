ab4cc477382355e9690a3fbc9d9b53b41a8922a8
taliamoszko <taliamoszko@gmail.com>
2024-11-12 00:07:30 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -3,7 +3,7 @@ public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
 		int balance = Integer.parseInt(args[0]);
-		double interest = ((double) Integer.parseInt(args[1])) /100.0;
+		double interest = (Double.parseDouble(args[1])) /100.0;
 		int n = Integer.parseInt(args[2]);
 		
 		double futureValue = balance * Math.pow(1 + interest, n);
